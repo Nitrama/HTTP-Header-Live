51f9af5dec454cf1587969437142c78d445f89d0
Martin Antrag <Martin.Antrag@gmail.com>
2018-06-30 00:05:51 +0200

    Change Size from Window, Added update URL

--- background.js
+++ background.js
@@ -2,9 +2,16 @@
 function NewWindow() {
 	windowscreate = chrome.windows.create({
 		height:600,
-		width:600,
+		width:800,
 		type:"popup",
 		url:chrome.extension.getURL("HTTPHeaderMain.html")
-	});
+    });
 }
 chrome.browserAction.onClicked.addListener(NewWindow);	
+
+function handleInstalled(details) {
+    browser.tabs.create({
+        url: "https://github.com/Nitrama/HTTP-Header-Live/blob/master/release/README.md"
+    });
+}
+browser.runtime.onInstalled.addListener(handleInstalled);
